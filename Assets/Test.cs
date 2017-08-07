eccea160a8f9069807b4a4d587fe51add6f69587
laijingfeng <337292616@qq.com>
2017-08-07 20:55:41 +0800

    init

---
+++ Assets/Test.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Threading;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+    }
+
+    public string fileName;
+
+    [ContextMenu("Do")]
+    private void Do()
+    {
+        Compress.CompressFile(Application.dataPath + "/../" + fileName);
+        AssetDatabase.Refresh();
+    }
+
+    public string pp;
+
+    [ContextMenu("UnDo")]
+    private void UnDo()
+    {
+        pp = Application.dataPath + "/../" + fileName;
+        Thread m_DecompressThread = null;
+        m_DecompressThread = new Thread(new ThreadStart(DoDO));
+        m_DecompressThread.Start();  
+        
+        //AssetDatabase.Refresh();
+    }
+
+    private void DoDO()
+    {
+        CodeProgress co = new CodeProgress(AA);
+        Compress.DecompressFile(pp, null, co);
+    }
+
+    private void AA(Int64 a, Int64 b)
+    {
+        Debug.Log("xx " + a + " " + b);
+    }
+}
